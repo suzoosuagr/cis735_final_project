180018f7822e210037e55f7133ba231f3cb18a8d
suzoosuagr <suzoosugar@gmail.com>
2021-05-11 01:39:30 -0400

    mlp for domain

--- Model/models.py
+++ Model/models.py
@@ -7,8 +7,8 @@ class DANN_resnet34(nn.Module):
         backbone = models.resnet34(pretrained)
         self.features = nn.Sequential(*list(backbone.children())[:-1])
         fc_in_dim = list(backbone.children())[-1].in_features
-        self.classifier = nn.Linear(fc_in_dim, nclass)
-        self.siamese = nn.Linear(fc_in_dim*2, 2)
+        self.classifier = MLP_Classifier(fc_in_dim, nclass)                  # 512, 
+        self.domain = MLP_Classifier(fc_in_dim*2, 2)
 
     def forward(self, img_0, img_1, alpha):
         if alpha is None:
@@ -21,8 +21,8 @@ class DANN_resnet34(nn.Module):
         sia_feat = torch.cat([feat_0, feat_1], dim=-1)
         reverse_feat = ReverseLayerF.apply(sia_feat, alpha)
 
-        sia_output = self.siamese(reverse_feat)
-        return class_output, sia_output
+        domain_output = self.domain(reverse_feat)
+        return class_output, domain_output
 
     def cls_forward(self, img_0):
         feat_0 = self.features(img_0).squeeze(2).squeeze(2)
@@ -30,14 +30,13 @@ class DANN_resnet34(nn.Module):
         return class_output
 
 
-class DANN_resnet50(nn.Module):
+class RevSiamese_resnet34(nn.Module):
     def __init__(self, nclass, pretrained) -> None:
-        super(DANN_resnet34, self).__init__()
-        backbone = models.resnet50(pretrained)
+        super(RevSiamese_resnet34, self).__init__()
+        backbone = models.resnet34(pretrained)
         self.features = nn.Sequential(*list(backbone.children())[:-1])
         fc_in_dim = list(backbone.children())[-1].in_features
-        self.classifier = nn.Linear(fc_in_dim, nclass)
-        self.domain = nn.Linear(fc_in_dim*2, 2)
+        self.classifier = MLP_Classifier(fc_in_dim, nclass)
 
     def forward(self, img_0, img_1, alpha):
         if alpha is None:
--- Model/networks.py
+++ Model/networks.py
@@ -14,3 +14,22 @@ class ReverseLayerF(Function):
     def backward(ctx, grad_output):
         output = grad_output.neg() * ctx.alpha
         return output, None
+
+class MLP_Classifier(nn.Module):
+    """
+        The easy linear classifier at the end. 
+    """
+    def __init__(self, in_ch, out_ch) -> None:
+        super(MLP_Classifier, self).__init__()
+        self.in_ch = in_ch
+        self.out_ch = out_ch
+        self.fc = nn.Sequential(
+            nn.Linear(in_ch, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, out_ch)
+        )
+
+    def forward(self, x):
+        return self.fc(x)
